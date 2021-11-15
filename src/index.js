49bd144fc03013266ccd0207d9490775a27c2e2f
Andrés <usuario616@gmail.com>
2021-11-15 01:18:34 +0100

    Changed uri

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import Pages from './pages';
 import { ApolloProvider, ApolloClient, InMemoryCache } from '@apollo/client';
 
 const client = new ApolloClient({
-  uri: 'http://localhost:4000',
+  uri: 'https://apollo-learning5t345345.herokuapp.com',
   cache: new InMemoryCache(),
 });
 
